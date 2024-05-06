57f078895e969b7ba314ce3d5cc94a551a8d2be7
Sachin Sakhare <sachinsakh108@gmail.com>
2024-05-06 13:10:28 +0530

    adding getdata api

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ dotenv.config(); // Load environment variables
 app.use(express.json());
 
 const pool = require('./config/db'); // Import the connection pool
+app.get('/', async (req,res) => {
+  try {
+    console.log("DFdsfd=======>");
+    const data = await pool.query('SELECT * FROM send_email')
+    if(!data){
+        return res.status(404).send({
+            success:false,
+            message:'No Records Found'
+        })
+    }
+    res.status(200).send({
+        success:true,
+        message:'No Records found',
+        data
+    })
+} catch (error) {
+    console.log(error)
+    res.status(500).send({
+        success:false,
+        message:'Error in Get All Student API',
+        error
+    })
+}
+})
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
